9c86da3ba40013cf0abd279ffa21b42e6fc40cb9
Nikita Bansode <bansodenikita66@gmail.com>
2026-09-11 11:28:36 +0530

    variables in python

---
+++ Basics/1_variables.py
@@ -0,0 +1,21 @@
+age=21
+name="Nikita"
+salary=0
+is_student=True
+
+print("Hieee !!! Welcome to my first ython code")
+print("Age : ",age)
+print("Name : ",name)
+print("Salary : ",salary)
+print("Is Student",is_student)
+
+age+=1
+print("Age : ",age)
+first_name="Nikita"
+last_name="Bansode"
+
+full_name=first_name+" "+last_name
+
+print("Full Name",full_name)
+
+print(f"My first name is {first_name} and last name is {last_name}")
